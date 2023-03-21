8c43ecd566068b0aea6c9427d44543d6698cb683
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-03-21 11:28:35 +0100

    fetch pokemons

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
 const pokemonNumber = 150;
-const url = '"https://pokeapi.co/api/v2/pokemon/';
+const url = 'https://pokeapi.co/api/v2/pokemon/';
+const pokemons = [];
+const container = document.querySelector('.container');
 
 function getPokemons() {
-  for (let i = 0; i < pokemonNumber; i++) {
-    fetch;
+  for (let i = 1; i <= pokemonNumber; i++) {
+    fetch(url + `${i}`)
+      .then((res) => res.json())
+      .then((pokemon) => {
+        pokemons.push(pokemon);
+        createPokemonCard(pokemon);
+      });
   }
 }
+
+function createPokemonCard(pokemon) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `
+        <div class="img-container">
+            <img class="img" src="${pokemon.sprites.front_default}" alt="${pokemon.name}"></img>
+        </div> 
+        <h2 class="name">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+    `;
+  container.appendChild(card);
+}
+
+getPokemons();
